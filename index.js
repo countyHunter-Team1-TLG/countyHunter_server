0ea99a0b9940b733a560ded042fe1ad9a55f5b8f
wang.xander <wang.xander@gmail.com>
2021-04-27 10:02:02 -0700

    Updated database and server

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-alert("Team1 county hunter server");
+const app = require("./services/server");
+const { MongoClient } = require("mongodb");
+const Users = require("./database/users");
+
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(
+  process.env.DB_URI,
+  // Set the poolSize to 50 connections.
+  // Set the write timeout limit to 5000 milliseconds.
+  {
+    useNewUrlParser: true,
+    poolSize: 50,
+    connectTimeoutMS: 5000,
+    useUnifiedTopology: true,
+    // other options can go here
+  }
+)
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await Users.injectDB(client);
+    //await another class object.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
