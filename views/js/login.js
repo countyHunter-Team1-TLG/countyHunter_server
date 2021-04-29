82135038a5b737ab548be199dd70a2a8d822d8b1
wang.xander <wang.xander@gmail.com>
2021-04-29 15:55:19 -0700

    Updated register send response json

--- views/js/login.js
+++ views/js/login.js
@@ -1,11 +1,14 @@
 const serverUrl = "https://countyhunter.herokuapp.com/user/login";
 // login button
 var form = document.forms.namedItem("user_login_form");
-form.addEventListener("submit", async function (ev) {
-  let formData = new FormData(form);
-  await axios.post(serverUrl, formData).then((response) => {
-    console.log(response);
+
+document
+  .getElementById("user_login_button")
+  .addEventListener("click", async function (ev) {
+    let formData = new FormData(form);
+    await axios.post(serverUrl, formData).then((response) => {
+      console.log(response);
+    });
   });
-});
 
 //
