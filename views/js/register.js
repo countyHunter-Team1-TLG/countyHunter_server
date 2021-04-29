7575010e45bfd0db7a48e230874249104f1cd2b3
Julian Dahlstet <jdahlstet@hotmail.com>
2021-04-29 15:18:49 -0700

    register.js

---
+++ views/js/register.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function () {
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  axios.post("/user/register", data).then((res) => {
+    console.log(res);
+  });
+});
