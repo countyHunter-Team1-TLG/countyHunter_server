bb0bc8ab28298f39c977f5b5bf9fdde495ab398a
wang.xander <wang.xander@gmail.com>
2021-04-30 10:12:03 -0700

    Updated legend position

--- views/js/runAnalysis.js
+++ views/js/runAnalysis.js
@@ -197,7 +197,7 @@ function displayData(data) {
     .append("g")
     .append("text")
     .attr("x", function (d, i) {
-      return 40 + i * 100;
+      return 40 + i * 200;
     })
     .attr("y", 30)
     .text(function (d) {
